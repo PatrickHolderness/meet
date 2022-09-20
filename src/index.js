7ce1ced0f9bed957df1870da3955299805396791
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2022-09-20 13:33:51 +0200

    Integration testing

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode>
+root.render(
+<React.StrictMode>
   <App />
 </React.StrictMode>
 );
