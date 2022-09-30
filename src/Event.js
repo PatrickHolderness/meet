142da644b198ca314302d2fa01a66eef29ca33f7
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2022-09-30 15:20:07 +0200

    Update Event.js

--- src/Event.js
+++ src/Event.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 
 class Event extends Component {
+
   toggleDetails = () => {
       let string = this.state.show ? 'View details' : 'Hide details';
       this.setState({ buttonText: string, show: !this.state.show });
   }
-
-  
 
   constructor() {
       super();
